c6feb2d6cb7019f40eb8c110371b2682296de776
hilal masri <hilal1922001@gmail.com>
2021-03-28 01:43:48 +0300

    load/save

--- tasks.js
+++ tasks.js
@@ -9,7 +9,7 @@
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', onDataReceived);
@@ -34,68 +34,52 @@ function startApp(name){
  * @returns {void}
  */
 
-var tasks=['hello','quit','exit','help'];
-var tasksD = [
-  {
-    name: "hello",
-    done: true
-  },
-  {
-    name: "quit",
-    done: false
-  },
-  {
-    name: "exit",
-    done: true
-  },
-  {
-    name: "help",
-    done: false
-  }
-]
-
+var tasks = ['hello', 'quit', 'exit', 'help'];
+var fs=require('fs');
+var data=fs.readFileSync('database.json','utf-8');
+var tasksD=JSON.parse(data);
 function onDataReceived(text) {
-  
-  if (text.trim() === 'quit'||text.trim()==='exit') {
+
+  if (text.trim() === 'quit' || text.trim() === 'exit') {
     quit();
   }
-  else if(text.trim().split(" ",1) == 'hello' || text.trim()==='hello'){
-   if(text.trim()==='hello'){
-     var tt="";
-     hello(tt);
-   }else{
-    var nameUser=text.trim().substring((text.trim()).lastIndexOf(" ")+1);
-    hello(nameUser);
-   }
+  else if (text.trim().split(" ", 1) == 'hello' || text.trim() === 'hello') {
+    if (text.trim() === 'hello') {
+      var tt = "";
+      hello(tt);
+    } else {
+      var nameUser = text.trim().substring((text.trim()).lastIndexOf(" ") + 1);
+      hello(nameUser);
+    }
   }
-  else if(text.trim()==='help' || text.trim().split(" ",1) == 'help'){
+  else if (text.trim() === 'help' || text.trim().split(" ", 1) == 'help') {
     help();
   }
-  else if(text.trim()==='list'){
+  else if (text.trim() === 'list') {
     list(tasksD);
   }
-  else if(text.trim().split(" ",1)=='add'){
+  else if (text.trim().split(" ", 1) == 'add') {
     var element;
-    element=text.trim().split(" ").pop().trim();
-    add(element,tasksD)
+    element = text.trim().split(" ").pop().trim();
+    add(element, tasksD)
   }
-  else if(text.trim().split(" ",1)=='remove'){
-    var re=text.trim().split(" ").pop().trim();
-    remove(re,tasksD);
+  else if (text.trim().split(" ", 1) == 'remove') {
+    var re = text.trim().split(" ").pop().trim();
+    remove(re, tasksD);
   }
-  else if(text.trim().split(" ",1)=='edit'){
-    var ed=text.trim().split(" ");
-    edit(ed,tasksD);
+  else if (text.trim().split(" ", 1) == 'edit') {
+    var ed = text.trim().split(" ");
+    edit(ed, tasksD);
   }
-  else if(text.trim().split(" ",1)=='check'){
-    var che=text.trim().split(" ");
-    check(tasksD,che);
+  else if (text.trim().split(" ", 1) == 'check') {
+    var che = text.trim().split(" ");
+    check(tasksD, che);
   }
-  else if(text.trim().split(" ",1)=='uncheck'){
-    var chec=text.trim().split(" ");
-    uncheck(tasksD,chec);
+  else if (text.trim().split(" ", 1) == 'uncheck') {
+    var chec = text.trim().split(" ");
+    uncheck(tasksD, chec);
   }
-  else{
+  else {
     unknownCommand(text);
   }
 }
@@ -108,8 +92,8 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"')
 }
 
 
@@ -118,12 +102,12 @@ function unknownCommand(c){
  *
  * @returns {void}
  */
-function hello(nameUser){
-  if(nameUser===""){
+function hello(nameUser) {
+  if (nameUser === "") {
     console.log('hello!');
   }
-  else{
-  console.log('hello '+nameUser+'!')
+  else {
+    console.log('hello ' + nameUser + '!')
   }
 }
 
@@ -133,7 +117,10 @@ function hello(nameUser){
  *
  * @returns {void}
  */
-function quit(){
+function quit() {
+  var fs = require('fs');
+  var data = JSON.stringify(tasksD);
+  fs.writeFileSync('database.json', data);
   console.log('Quitting now, goodbye!')
   process.exit();
 }
@@ -142,7 +129,7 @@ function quit(){
  * 
 @returns {void}
 */
-function help(){
+function help() {
   console.log("hello ,quit or exit,list,add,remove,check,uncheck");
 }
 
@@ -150,49 +137,49 @@ function help(){
  * 
  * @returns {void}
  */
-function list(tasksD){
-for(let i=0;i<tasksD.length;i++){
-  if(tasksD[i].done){
-    console.log("[✓] "+tasksD[i].name)
+function list(tasksD) {
+  for (let i = 0; i < tasksD.length; i++) {
+    if (tasksD[i].done) {
+      console.log("[✓] " + tasksD[i].name)
+    }
+    else {
+      console.log("[ ] " + tasksD[i].name)
+    }
   }
-  else{
-    console.log("[ ] "+tasksD[i].name)
-  }
-}
 }
 /**
  * 
  * @returns {void}
  * 
  */
-function add(element,tasksD){
-if(element==='add'){
-  console.log("error");
-}
-else{
-  let newTask={
-    name:element.trim(),
-    done:false
-  };
- tasksD.push(newTask);
- console.log(element+" is added");
-}
+function add(element, tasksD) {
+  if (element === 'add') {
+    console.log("error");
+  }
+  else {
+    let newTask = {
+      name: element.trim(),
+      done: false
+    };
+    tasksD.push(newTask);
+    console.log(element + " is added");
+  }
 }
 /**
  * @returns {void}
  */
-function remove(re,tasksD){
-  if(re==='remove'){
+function remove(re, tasksD) {
+  if (re === 'remove') {
     tasksD.pop()
     console.log("the last tasks is deleted")
   }
-  else{
-    if(re>=0 && re<=tasks.length){
-    tasksD.splice(parseInt(re)-1,1);
-    console.log("tasks"+re+" is deleted");
+  else {
+    if (re >= 0 && re <= tasks.length) {
+      tasksD.splice(parseInt(re) - 1, 1);
+      console.log("tasks" + re + " is deleted");
     }
-    else{
-      console.log(re+" is not exist")
+    else {
+      console.log(re + " is not exist")
     }
   }
 }
@@ -200,44 +187,45 @@ function remove(re,tasksD){
 /**
  * @returns {void}
  */
-function edit(re,tasksD){
-  if(re.length==1){
+function edit(re, tasksD) {
+  if (re.length == 1) {
     console.log("error");
   }
-  else if(re.length==2){
-    tasksD[tasksD.length-1].name=re[1];
+  else if (re.length == 2) {
+    tasksD[tasksD.length - 1].name = re[1];
     console.log("last tasks edited")
   }
-  else if(re.length==3){
-    tasksD[re[1]-1].name=re[2];
-    console.log("task "+re[1]+" edited")
+  else if (re.length == 3) {
+    tasksD[re[1] - 1].name = re[2];
+    console.log("task " + re[1] + " edited")
   }
 }
 /**
  * 
  * @returns {void}
  */
- function check(tasksD,che){
-   if(che.length==1){
-     console.log("error");
-   }
-   else{
-     tasksD[che[1]-1].done=true;
-   }
- }
-
- /**
-  * @returns {void}
-  */
- function uncheck(tasksD,chec){
-  if(chec.length==1){
+function check(tasksD, che) {
+  if (che.length == 1) {
     console.log("error");
   }
-  else{
-    tasksD[chec[1]-1].done=false;
+  else {
+    tasksD[che[1] - 1].done = true;
   }
 }
- 
+
+/**
+ * @returns {void}
+ */
+function uncheck(tasksD, chec) {
+  if (chec.length == 1) {
+    console.log("error");
+  }
+  else {
+    tasksD[chec[1] - 1].done = false;
+  }
+}
+
+
 
 
 // The following line starts the application
